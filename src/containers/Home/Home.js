d3951860d5be5fbc7d7b814001e66b633e9440b1
fergusKe <fergus.jingtai@gmail.com>
2018-06-18 10:08:01 +0800

    init

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React, {
+  Component
+} from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div id="app">
+        Hello Worlds
+      </div>
+    );
+  }
+}
+
+export default Home;
